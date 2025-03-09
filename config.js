c1f39716c5101dcaa500299d30d254a8e64bcf4c
Sunil Chow <Sunil.c28c@gmail.com>
2025-03-09 17:20:10 -0400

    Version 13

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 THis is version 2 of config.js
 This is version 11 after pushing to git hub
 This is version 12 after amending version 11
+This is version 13 added in new local repository
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 this is version 1 in index.js
 updated this file with HEAD in version 3 but checking out from version 1
 updated this in version 12
+this is added in version 13 of new local repository
